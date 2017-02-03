5cb1b9554a6abe7c2f0da90f57b535a7932e5ffc
Vanshdeep <coolx91@gmail.com>
2017-02-02 20:53:12 -0500

    Add files via upload

---
+++ tricky_question_2.py
@@ -0,0 +1,24 @@
+# write a program in which a function takes a string argument
+# that returns the first letter of every word in capitals
+# >>> 'hello i'm fine'
+#      Hello I'm Fine
+
+def LetterCapitalization(a):
+
+    words = a.split(' ')
+
+    for i in range(len(words)):
+        words[i] = words[i][0].upper() + words[i][1:] 
+
+    return ' '.join(words)
+
+print(LetterCapitalization('hello how are you my boy'))
+
+# Alternate Method
+ a= "hello i'm fine"
+
+ w = a.title()
+ print(w)
+                
+# .title() is a beautiful object method that returns
+# the first letter of every word or every word in a sentence in capitals.     
